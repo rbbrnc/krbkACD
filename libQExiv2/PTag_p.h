f96059c01c9b694c9e04720af78da117d251f7c2
Roberto Branciforti <rbbrnc@gmail.com>
2012-09-16 00:09:39 +0200

    Remove unused members.

--- libQExiv2/PTag_p.h
+++ libQExiv2/PTag_p.h
@@ -15,20 +15,9 @@ class PTagDataPrivate : public QSharedData
 
 	public:
 		unsigned int	m_type;		// MP or MWG
+
 		QString		m_text;		// Description
 		QRectF		m_region;	// Normalized [0, 1] region rect
-
-		QString m_mwgName;
-		QString m_mwgDescription;
-		QString m_mwgType;		// "Face" | "Pet" | "Focus" | "BarCode"
-		QString m_mwgBarcodeValue;
-
-		double m_mwgStAreaX;	// X coordinate of the center of the area (point, circle, rectangle)
-		double m_mwgStAreaY;	// Y coordinate of the center of the area (point, circle, rectangle)
-		double m_mwgStAreaW;	// Width of the area (rectangle)
-		double m_mwgStAreaH;	// Height of the area (rectangle)
-		double m_mwgStAreaD;	// Diameter of area (circle)
-		QString m_mwgStAreaUnit; // Only "normalized"
 
 		MwgRegionInfo m_mwgRegionInfo;
 		MPRegionInfo  m_mpRegionInfo;
