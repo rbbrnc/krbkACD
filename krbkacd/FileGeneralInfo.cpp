cfa483245ed036f68b0dbcc07279af4bee9d5ad4
RB <rbbrnc@gmail.com>
2012-10-01 12:55:55 +0200

    Put here preview and size

--- krbkacd/FileGeneralInfo.cpp
+++ krbkacd/FileGeneralInfo.cpp
@@ -12,6 +12,10 @@ FileGeneralInfo::FileGeneralInfo(const QFileInfo &fileInfo, QWidget *parent)
 	ui->fileName->setText(fileInfo.fileName());
 	ui->filePath->setText(fileInfo.absoluteFilePath());
 
+	QPixmap thumbnail(fileInfo.fileName());
+	ui->thumbnail->setPixmap(thumbnail);
+	ui->imageDimension->setText(QString::number(thumbnail.width()) + "x" + QString::number(thumbnail.height()));
+
 	ui->fileSize->setText(QString("%1 bytes").arg(fileInfo.size()));
 
 	ui->fileOwner->setText(fileInfo.owner());
