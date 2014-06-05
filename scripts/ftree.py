8e6be962e66533b8d01fd0477211926e3ee64855
RB <rbbrnc@gmail.com>
2014-06-05 17:57:40 +0200

    Add argparse

--- scripts/ftree.py
+++ scripts/ftree.py
@@ -4,6 +4,12 @@ import hashlib    #-- For md5
 import subprocess #-- For exec shell commands
 
 #----------------------------------------------------------------
+#-- Get mime type of filename using 'file'
+#-----------------------------------------------------------------
+def get_mime_type(filename):
+	return subprocess.check_output(['file', '-b', '--mime-type', filename]).rstrip('\n')
+
+#----------------------------------------------------------------
 #-- Calc th md5 sum of filename
 #-----------------------------------------------------------------
 def md5sum(filename):
@@ -21,12 +27,19 @@ def md5sum(filename):
 #-- count + md5sum + file + mime type
 #-----------------------------------------------------------------
 def traverse(startdir):
+	#-- Check if is a valid dir.
+	if not os.path.isdir(startdir):
+		raise Exception(startdir + " path don't exists")
+
+	#-- Check if is an absolute path.
+	if not os.path.isabs(startdir):
+		raise Exception(startdir + " is not an absolute path!")
+
 	count = 0
 	for root, dirs, files in os.walk(startdir):
 		for name in files:
 			fn = os.path.join(root, name)
-			mime = subprocess.check_output(['file', '-b', '--mime-type', fn])
-			print str(count) + ";" + md5sum(fn) + ";\"" + fn + "\";" + mime.rstrip('\n')
+			print str(count) + ";" + md5sum(fn) + ";\"" + fn + "\";" + get_mime_type(fn)
 			count += 1;
 
 #----------------------------------------------------------------
@@ -34,19 +47,13 @@ def traverse(startdir):
 #----------------------------------------------------------------
 if __name__ == "__main__":
 
-	try:
-		cwd = sys.argv[1]
-	except:
-		cwd = os.getcwd()
+	import argparse
 
-	#-- Check if is a valid dir.
-	if os.path.isdir(cwd):
-		#-- Check if is an absolute path.
-		if os.path.isabs(cwd):
-			#-- Do the job.
-			traverse(cwd)
-		else:
-			print cwd + " is not an absolute path!"
-	else:
-		print cwd + " path don't exists"
+	parser = argparse.ArgumentParser(description='Traverse FileSystem', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('-d', '--dir', required=False, metavar='<start_dir>', default=os.getcwd(), help=u'Start directory')
+#	parser.add_argument('-o', '--out', required=False, metavar='<output_file>', default=os.getcwd(), help=u'Start directory')
+	args = parser.parse_args()
+
+	traverse(args.dir)
+
 
